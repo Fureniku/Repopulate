7d2e6978315b84750264c47d60b68f4aa55d85bd
Fureniku <fureniku@gmail.com>
2023-10-07 16:42:21 +0100

    Honing in on scale (most work today in a google sheet)

--- Assets/Scripts/World/Celestial/CelestialBodyController.cs
+++ Assets/Scripts/World/Celestial/CelestialBodyController.cs
@@ -4,22 +4,52 @@ using UnityEngine;
 
 public class CelestialBodyController : MonoBehaviour {
 
-    private SolarSystemManager solarSystem;
-    private CelestialBody celestialBody;
+    private SolarSystemManager _solarSystem;
+    private CelestialBody _celestialBody;
+
+    private Transform _ship;
+
+    private float _closestDistance;
     
     [Tooltip("The rotation/orbit speed in degrees per second. 1 = 5 mins for one orbit (year)")]
-    [SerializeField] private float rotationSpeed = 1f; // Adjust this to change the rotation speed (degrees per second)
+    [SerializeField] private float _orbitSpeed = 1f; // Adjust this to change the rotation speed (degrees per second)
+    
+    [Header("Dynamic Rescaling")]
+    [SerializeField] private bool _canDynamiclyRescale = true;
+    [Tooltip("The distance where scale will be at maximum, and no longer increase")]
+    [SerializeField] private float _minDistance = 5000f;
+    [Tooltip("The distance at which scale for the object would be minimum.")]
+    [SerializeField] private float _maxDistance = 100000f;
+    [Tooltip("The distance from the *planets surface* (not centre) where scale should be max")]
+    [SerializeField] private float _scaleMax;
+    [SerializeField] private float _scaleMin;
     
     void Start() {
-        solarSystem = GameManager.Instance.GetSolarSystem();
-        celestialBody = GetComponentInChildren<CelestialBody>();
+        _solarSystem = GameManager.Instance.GetSolarSystem();
+        _celestialBody = GetComponentInChildren<CelestialBody>();
+        _ship = GameManager.Instance.GetShipController().transform;
+        _closestDistance = _minDistance/2 + _scaleMax;
     }
 
     void FixedUpdate() {
-        if (rotationSpeed >= 0) {
-            float rotationAmount = rotationSpeed * Time.deltaTime;
+        if (_orbitSpeed >= 0) {
+            float rotationAmount = _orbitSpeed * Time.deltaTime;
             transform.Rotate(Vector3.up, rotationAmount);
         }
+    }
+
+    void Update() {
+        if (_canDynamiclyRescale) {
+            _celestialBody.transform.localScale = GetScale();
+        }
+    }
+
+    //Adjust the scale based on distance
+    protected virtual Vector3 GetScale() {
+        float dist = Vector3.Distance(_ship.position, _celestialBody.transform.position);
+        float scaledDistance = Mathf.Clamp01((dist - _closestDistance) / (_maxDistance - _closestDistance));
+        float scale = Mathf.Lerp(_scaleMax, _scaleMin, scaledDistance);
+        return Vector3.one * scale;
     }
 
     public static void create(ref GameObject gameObject) {
--- Assets/Scripts/World/Station/StationPhysics.cs
+++ Assets/Scripts/World/Station/StationPhysics.cs
@@ -10,16 +10,22 @@ public class StationPhysics : MonoBehaviour {
     [SerializeField] private Vector3 inertiaTensor;
 
     [SerializeField] private GameObject ship;
+
+    [SerializeField] private Vector3 startingVelocity;
     
     void Start() {
         rb = GetComponent<Rigidbody>();
 
         rb.centerOfMass = centreMass;
         rb.inertiaTensor = inertiaTensor;
+
+        
     }
 
     void Update() {
         ship.transform.position = transform.position;
         ship.transform.rotation = transform.rotation;
+        
+        rb.velocity = startingVelocity;
     }
 }
